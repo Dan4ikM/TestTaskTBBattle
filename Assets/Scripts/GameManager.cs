81685a9579f3d3b577e6629a6ee9ba27b5fa5f1f
DaniilMoi <moiseev@179.ru>
2023-10-05 16:12:09 +0300

    Add buffs, prefabs and base UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,14 +11,18 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Unit unitRight;
     [SerializeField] private Button unitLeftAttackButton;
     [SerializeField] private Button unitRightAttackButton;
+    [SerializeField] private Button unitLeftApplyRandomBuff;
+    [SerializeField] private Button unitRightApplyRandomBuff;
     [SerializeField] private Button restartButton;
     [SerializeField] private TMP_Text roundNumberText;
 
+    private BuffManager buff_manager;
     private Unit currentUnit;
     private int roundNumber;
-        
+
     private void Awake()
     {
+        buff_manager = GetComponent<BuffManager>();
         Unit.OnAnyDead += Restart;
         unitLeft.OnAttackEnded += OnAttackEnded_EndTurn;
         unitRight.OnAttackEnded += OnAttackEnded_EndTurn;
@@ -26,6 +30,8 @@ public class GameManager : MonoBehaviour
         restartButton.onClick.AddListener(Restart);
         unitLeftAttackButton.onClick.AddListener( () => unitLeft.Attack(unitRight));
         unitRightAttackButton.onClick.AddListener(() => unitRight.Attack(unitLeft));
+        unitLeftApplyRandomBuff.onClick.AddListener(()=> unitLeft.ApplyRandomBuff(buff_manager));
+        unitRightApplyRandomBuff.onClick.AddListener(()=> unitRight.ApplyRandomBuff(buff_manager));
     }
 
     private void Start()
@@ -36,18 +42,26 @@ public class GameManager : MonoBehaviour
     private void OnAttackEnded_EndTurn()
     {
         Debug.Log("End turn");
-
+        //currentUnit.CheckBuffsDuration
         if (currentUnit != unitRight) {} else EndRound();
         
+        //Change unit
         currentUnit = currentUnit == unitLeft ? unitRight : unitLeft;
-        currentUnit.IsTurn = true;
+
+        StartNewTurn();
         Debug.Log("New turn");
-        
+        //currentUnit.BuffsOnNewRoundCheck
         void EndRound()
         {
             Debug.Log("End round " + roundNumber);
             StartNewRound();
         }
+    }
+
+    void StartNewTurn()
+    {
+        currentUnit.IsTurn = true;
+        currentUnit.IsApplyBuffOnThisTurn = false;
     }
     
     void StartNewRound()
@@ -63,11 +77,13 @@ public class GameManager : MonoBehaviour
         unitRight.Initialize();
 
         unitLeft.IsTurn = true;
+        unitLeft.IsApplyBuffOnThisTurn = false;
         unitRight.IsTurn = false;
 
         currentUnit = unitLeft;
         roundNumber = 0;
         StartNewRound();
+        StartNewTurn();
     }
     
     private void Restart()
---
+++ Assets/Scripts/Unit/Attributes/IAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IAttribute
+{
+    public event EventHandler OnAttributeChanged;
+
+    public int GetAttributeNormalized();
+
+    public int GetAttributeValue();
+}
